e4043ec86e77e31658b72c3b80cada37becbfb4b
Jason <ironelder@gmail.com>
2019-11-24 14:12:12 +0900

    Application에 Dagger Factory추가 및 RegistrationActivity에 의존성 주입
    Application에 Dagger Factory추가 및 RegistrationActivity에 의존성 주입

--- app/src/main/java/com/ironelder/codelabdagger/MyApplication.kt
+++ app/src/main/java/com/ironelder/codelabdagger/MyApplication.kt
@@ -1,10 +1,17 @@
 package com.ironelder.codelabdagger
 
 import android.app.Application
+import com.ironelder.codelabdagger.di.AppComponent
+import com.ironelder.codelabdagger.di.DaggerAppComponent
 import com.ironelder.codelabdagger.storage.SharedPreferencesStorage
 import com.ironelder.codelabdagger.user.UserManager
 
 open class MyApplication : Application() {
+
+    val appComponent:AppComponent by lazy {
+        DaggerAppComponent.factory().create(applicationContext)
+    }
+
 
     open val userManager by lazy {
         UserManager(SharedPreferencesStorage(this))
